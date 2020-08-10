9ba81c6f222a3a4b3adaef6f88808ee1b834edf2
Werewolf <54082165+Werewolf7@users.noreply.github.com>
2020-08-10 20:37:35 +0300

    create

---
+++ names_of_publishing_houses.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+class Publishing {
+private:
+    std::string name;
+    int price;
+public:
+    std::string get_name() const;
+    int get_price() const;
+    void set_name(std::string name);
+    void set_price(int price);
+    friend std::istream& operator >> (std::istream& in, Publishing& pbl);
+    friend std::ostream& operator << (std::ostream& out, const Publishing& pbl);
+};
+
+std::string Publishing::get_name() const { return this->name; }
+int Publishing::get_price() const { return this->price; }
+void Publishing::set_name(std::string name) { this->name = name; }
+void Publishing::set_price(int price) { this->price = price; }
+
+std::istream& operator >> (std::istream& in, Publishing& pbl) {
+    in >> pbl.name >> pbl.price;
+    return in;
+}
+
+std::ostream& operator << (std::ostream& out, const Publishing& pbl) {
+    out << pbl.name << std::endl << pbl.price << std::endl;
+    return out;
+}
+
+int main() {
+    std::vector<Publishing> arr(2);
+    std::cin >> arr[0] >> arr[1];
+    std::cout << arr[0] << arr[1];
+    auto res = std::min_element(arr.begin(), arr.end(), [](const Publishing& el, const Publishing& el_) { return el.get_price() < el_.get_price(); });
+    std::cout << std::endl << res->get_name();
+}
