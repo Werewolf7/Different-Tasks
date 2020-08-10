a1f14a24efc98182fdd414879d3db81856b558f3
Werewolf <54082165+Werewolf7@users.noreply.github.com>
2020-08-10 19:52:16 +0300

    create

---
+++ reverse_arr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+
+
+int main()
+{
+    std::vector<int> arr{ std::istream_iterator<int>(std::cin), std::istream_iterator<int>() };
+    reverse(arr.begin(), arr.end());
+    copy(arr.begin(), arr.end(), std::ostream_iterator<int>(std::cout, " "));
+}
