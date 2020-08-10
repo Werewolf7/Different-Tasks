802395fa383bf25554181be32497a11d1dc5184c
Werewolf <54082165+Werewolf7@users.noreply.github.com>
2020-08-10 21:46:39 +0300

    swap_min_max

---
+++ swap_min_max.cpp
@@ -0,0 +1,14 @@
+#include <array>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::array<int, 6> arr = { 1, 2, 5, 0, 3, 6 };
+    const auto [min_it, max_it] = minmax_element(begin(arr), end(arr));
+    iter_swap(min_it, max_it);
+
+    for (const auto& item : arr) {
+        cout << item << '\n';
+    }
+}
